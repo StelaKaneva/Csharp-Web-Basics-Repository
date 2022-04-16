c1ca3c8298e70553d4db20b90d7fe83a848f12e4
Stela Kaneva <70379843+StelaKaneva@users.noreply.github.com>
2022-04-16 15:41:56 +0300

    Add Exam Preparation - Git Exam

---
+++ Exams/Git/CSharp-Web-Server-main/Git/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace Git
+{
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using MyWebServer;
+    using MyWebServer.Controllers;
+    using MyWebServer.Results.Views;
+    using Git.Data;
+    using Git.Services;
+
+    public class StartUp
+    {
+        public static async Task Main()
+            => await HttpServer
+                .WithRoutes(routes => routes
+                .MapStaticFiles()
+                .MapControllers())
+            .WithServices(services => services
+                .Add<IViewEngine, CompilationViewEngine>()
+                .Add<IValidator, Validator>()
+                .Add<IPasswordHasher, PasswordHasher>()
+                .Add<GitDbContext>())
+            .WithConfiguration<GitDbContext>(context => context
+                 .Database.Migrate())
+            .Start();
+    }
+}
